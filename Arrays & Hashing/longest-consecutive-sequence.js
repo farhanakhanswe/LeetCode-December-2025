fad3823446ea5f4345c549b97ffefbb8d6357d78
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2025-12-08 00:00:04 +0600

    Update longest-consecutive-sequence.js

--- Arrays & Hashing/longest-consecutive-sequence.js
+++ Arrays & Hashing/longest-consecutive-sequence.js
@@ -5,32 +5,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-
-/* 
-   Leetcode accepted this soln but it does not meet the O(n) condition. Need to still make improvements.
-   The time complexity is O(nlogn) rn because of sorting the array.
-*/
-
 var longestConsecutive = function(nums) {
-    if(nums.length < 1) return 0;
-    let sortedArr = [...new Set(nums)].sort((a, b) => a - b); 
-    if(sortedArr.length === 1) return 1;
+    if(nums.length === 0) return 0;
 
-    let length = 1;
-    let count = 1;
+    const set = new Set(nums);
+    let length = 0;
 
-    for(let i = 1 ; i < sortedArr.length ; i++)
-    {
-        if(sortedArr[i - 1] + 1 === sortedArr[i]){
-            count++;
-        }else{
-            count = 1;
+    for(const num of set){
+
+        if(!set.has(num -1)) // start counting if the number is the start of the sequence
+        {
+            let currNum = num;
+            let count = 1;
+
+            while(set.has(currNum + 1)){
+                currNum++;
+                count++;
+            }
+
+            length = count > length ? count : length;
         }
-            
-        length = count > length ? count : length;
     }
 
     return length;
-    
  };
